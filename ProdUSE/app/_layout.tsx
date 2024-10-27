28a14e552bf426152bcc96d2f653ef1f156b701a
Christina Yang <121911810+ChristinaYang1@users.noreply.github.com>
2024-10-26 22:17:09 -0400

    Navigation and New .tsx files
    A simple one but it works

--- ProdUSE/app/_layout.tsx
+++ ProdUSE/app/_layout.tsx
@@ -1,15 +1,60 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function RootLayout() {
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen 
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: 'green', 
+      tabBarInactiveTintColor: 'gray', 
+      headerShown: false,
+      tabBarStyle: {
+        marginTop: 10, 
+      },
+      }}
+    >
+      <Tabs.Screen
         name="index" 
         options={{
-          // Hide the header for this route
-          headerShown: false,
+          title: 'My Fridge',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
         }}
       />
-    </Stack>
+      <Tabs.Screen
+        name="pantry" 
+        options={{
+          title: 'pantry',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'basket' : 'basket-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="metrics" 
+        options={{
+          title: 'metrics',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'analytics' : 'analytics-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile" 
+        options={{
+          title: 'profile',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+    
   );
 }
---
+++ ProdUSE/components/geocery.tsx
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+
+export default function Metrics() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.text}>Hello, this is the geocery page</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',  
+    alignItems: 'center',     
+    backgroundColor: '#f8f8f8', 
+  },
+  text: {
+    fontSize: 24, 
+    fontWeight: 'bold', 
+  },
+});
