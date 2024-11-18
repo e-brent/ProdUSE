45284b451aa2cb95a151a3bb3c77a014d9f757f1
kyle <kyle.harwick29@gmail.com>
2024-11-18 13:15:38 -0500

    updated operations
    added addPastItem and helper method

--- ProdUSE/db/operations.ts
+++ ProdUSE/db/operations.ts
@@ -4,8 +4,8 @@ import { PerishableItem, PastItem, Recipe } from './types';
 class Operations {
     constructor(private db: SQLiteDatabase) { }
 
-// get all items from the perishableItems table
-// Emme
+    // get all items from the perishableItems table
+    // Emme
     async getPerishableItems(): Promise<PerishableItem[]> {
 
         const rawValues = await this.db.getAllAsync<
@@ -22,20 +22,20 @@ class Operations {
         }));
     }
 
-// search perishableItems based on item name -- return list of matching items
-// Emme
+    // search perishableItems based on item name -- return list of matching items
+    // Emme
     async searchPerishableItems(item_name?: string): Promise<PerishableItem[]> {
 
         let query = "";
 
-        if (item_name == ""){
+        if (item_name == "") {
             query = 'SELECT perishable_id, perishable_name, date_purchased as "date_purchased", expiration_date as "expiration_date", days_in_fridge, amount_used, category, image_url FROM perishableItems';
         }
         else {
             query = `SELECT perishable_id, perishable_name, date_purchased as "date_purchased", expiration_date as "expiration_date", days_in_fridge, amount_used, category, image_url FROM perishableItems WHERE perishable_name LIKE "${item_name}"`;
         }
 
-        console.log (query);
+        console.log(query);
 
         const rawValues = await this.db.getAllAsync<
             Omit<PerishableItem, 'date_purchased' | 'expiration_date'> & {
@@ -51,8 +51,8 @@ class Operations {
         }));
     }
 
-// get specific item from the perishableItems table by id -- will be used for the item details page
-// Emme
+    // get specific item from the perishableItems table by id -- will be used for the item details page
+    // Emme
     async getperishableItem(id: number): Promise<PerishableItem | null> {
         const rawValue = await this.db.getFirstAsync<
             Omit<PerishableItem, 'date_purchased' | 'expiration_date'> & {
@@ -71,8 +71,8 @@ class Operations {
         };
     }
 
-// get the last id in the perishableItems table -- helper function for adding new items to the table
-    async getLastId(): Promise<number | null> {
+    // get the last id in the perishableItems table -- helper function for adding new items to the table
+    async getLastPerishibleId(): Promise<number | null> {
 
         const id = await this.db.getFirstAsync<{ perishable_id: number }>('SELECT perishable_id from perishableItems ORDER BY perishable_id DESC');
 
@@ -83,26 +83,26 @@ class Operations {
         return id.perishable_id;
     }
 
-// get item expiration date from table -- this table needs to be created -- actually this is seeming very complicated so maybe not??
+    // get item expiration date from table -- this table needs to be created -- actually this is seeming very complicated so maybe not??
 
-// get category image from table -- helper function for adding new items to the table
-// Emme
+    // get category image from table -- helper function for adding new items to the table
+    // Emme
     async getImagePath(category: string): Promise<string | null> {
-        const imagePath = await this.db.getFirstAsync<{image_path: string}>('SELECT image_path FROM images WHERE category LIKE "?"', [category]);
+        const imagePath = await this.db.getFirstAsync<{ image_path: string }>('SELECT image_path FROM images WHERE category LIKE "?"', [category]);
 
-        if(!imagePath){
+        if (!imagePath) {
             return 'ProdUSE/assets/images/otherIcon.png';
         }
-        
+
         return imagePath.image_path;
     }
 
-// add item to perishableItems table
-// Emme
+    // add item to perishableItems table
+    // Emme
     async addPerishableItem(perishable_name: string, date_purchased: Date, amount_used: number, category: string): Promise<void> {
 
         // create item id
-        let id = await this.getLastId();
+        let id = await this.getLastPerishibleId();
         if (!id) {
             id = 0;
         }
@@ -124,9 +124,9 @@ class Operations {
 
     }
 
-// edit item in perishableItems table
-// edit amount_used
-// Kyle
+    // edit item in perishableItems table
+    // edit amount_used
+    // Kyle
     async editAmountUsed(perishable_id: number, amount_used: number): Promise<void> {
         const item = await this.getperishableItem(perishable_id);
 
@@ -136,8 +136,8 @@ class Operations {
         );
     }
 
-// filter by category
-// Kyle
+    // filter by category
+    // Kyle
     async filterByCategory(category: string): Promise<PerishableItem[]> {
 
         const rawValues = await this.db.getAllAsync<
@@ -159,8 +159,8 @@ class Operations {
         }));
     }
 
-// filter by date of purchase
-// Kyle
+    // filter by date of purchase
+    // Kyle
     async filterByDateOfPurchase(sortOrder: 'asc' | 'desc'): Promise<PerishableItem[]> {
         const rawValues = await this.db.getAllAsync<
             Omit<PerishableItem, 'date_purchased' | 'expiration_date'> & {
@@ -187,29 +187,61 @@ class Operations {
         const newestFirst = await operations.filterByDateOfPurchase('desc');
         */
     }
-    
-// filter by category
-// Kyle
-    
 
-// delete item from perishableItems
-// Emme
-    async deletePerishable(id: string):Promise<void>{
+
+
+    // delete item from perishableItems
+    // Emme
+    async deletePerishable(id: string): Promise<void> {
         await this.db.runAsync(
             'DELETE FROM perishableItems WHERE perishable_id = ?',
             [id]
         );
     }
 
-// add item to pastItems table
-// Emme
+    // helper function to get last past_id
+    async getLastPastId(): Promise<number | null> {
+        const id = await this.db.getFirstAsync<{ past_id: number }>(
+            'SELECT past_id from pastItems ORDER BY past_id DESC'
+        );
 
-// get all recipes
-// Kyle
+        if (!id) {
+            return null;
+        }
 
-// get recipes with specific ingredient
-// only one parameter (item name) should return a list of recipes
-// Kyle
+        return id.past_id;
+    }
+
+    // add item to pastItems table
+    // similar to addPerishible, for when item is thrown out and it changed in as a pastItem
+    // Emme
+    async addPastItem(past_name: string, date_purchased: Date, date_finished: Date, days_in_fridge: number, completed: boolean, total_used: number, category: string): Promise<void> {
+        let id = await this.getLastPastId();
+        if (!id) {
+            id = 0;
+        }
+        else {
+            id = id + 1;
+        }
+
+        const purchaseDate = date_purchased.toString();
+        const finishDate = date_finished.toString();
+
+        const completedInt = completed ? 1 : 0;
+
+        await this.db.runAsync(
+            'INSERT INTO pastItems (past_id, past_name, date_purchased, date_finished, days_in_fridge, completed, total_used, category) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [id, past_name, purchaseDate, finishDate, days_in_fridge, completedInt, total_used, category]
+        );
+    }
+
+
+    // get all recipes
+    // Kyle
+
+    // get recipes with specific ingredient
+    // only one parameter (item name) should return a list of recipes
+    // Kyle
     async getRecipesByIngredient(itemName: string): Promise<Recipe[]> {
         const recipes = await this.db.getAllAsync<Recipe>(
             'SELECT item, recipe_name FROM recipes WHERE item = ?',
@@ -224,8 +256,8 @@ class Operations {
         return recipes;
     }
 
-// delete recipes
-// Kyle
+    // delete recipes
+    // Kyle
     async deleteRecipe(item: string, recipeName: string): Promise<void> {
         // Check if recipe exists first
         const existingRecipe = await this.db.getFirstAsync<Recipe>(
@@ -243,8 +275,8 @@ class Operations {
         );
     }
 
-// operations for specific stats???
-// Emme
+    // operations for specific stats???
+    // Emme
 
 
 
